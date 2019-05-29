a7e3036a8970342564d626e052df2f1c6927276e
Phillip Nguyen <nh273@cornell.edu>
2019-05-29 18:07:16 -0400

    working state query by naics, years, geo codes

---
+++ src/load_data.py
@@ -0,0 +1,151 @@
+import os
+import pandas as pd
+import glob
+from tqdm import tqdm, tqdm_notebook
+
+MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
+FUNC_DATA = os.path.join(MODULE_PATH, os.pardir, 'data', 'functional')
+
+
+def _load_data_from_paths(paths, geo_level, column_set, notebook):
+    """Helper method to load CBP data from given list of file paths.
+
+    Arguments:
+        :: paths {list} -- list of paths to data that needed to be loaded
+        :: geo_level {str} -- 'msa' or 'county'
+        :: column_set {str} -- variables to load.
+        'all': load all available columns.
+        'strict': only load the most essential columns.
+        :: notebook {bool} -- is the module running in a Jupyter notebook?
+        The only purpose is to correctly display progress bar.
+        (default: True)
+    Return:
+        single pandas Dataframe of data from specified paths.
+    """
+    strict_column_set = {'msa': ['MSA', 'YEAR', 'NAICS', 'EMP', 'PAYANN', 'ESTAB'],
+                         'county': ['FIPS', 'YEAR', 'naics_level',
+                                    'naics', 'EMP', 'PAYANN', 'ESTAB']}
+
+    progress_bar = tqdm_notebook if notebook else tqdm
+    res = []
+
+    for path in progress_bar(paths):
+        try:
+            df = pd.read_csv(path, dtype='str')
+            if column_set == 'strict':
+                df = df[strict_column_set[geo_level]]
+            res.append(df)
+        except FileNotFoundError as e:
+            print(e)
+    return pd.concat(res)
+
+
+def load_by_year(years, geo_level='county', column_set='strict', notebook=True):
+    """
+    Load functional data only for selected years
+    column_set is 'strict': only a subset of columns present in all years
+    will be loaded or 'all': all columns are loaded
+
+    Arguments:
+        :: years {list of int or str} -- desired years.
+        :: geo_level {str} -- 'msa' or 'county' (default: 'county')
+        :: column_set {str} -- variables to load. 'all': load all available columns.
+        'strict': only load the most essential columns.
+        (default: 'strict')
+        :: notebook {bool} -- is the module running in a Jupyter notebook?
+        The only purpose is to correctly display progress bar.
+        (default: True)
+    Return:
+        pandas Dataframe of desired functional data.
+    """
+    paths = []
+    for year in years:
+        path = os.path.join(FUNC_DATA, 'year', geo_level,
+                            f'cbp_year_{geo_level}_{year}.csv')
+        paths.append(path)
+    return _load_data_from_paths(paths, geo_level, column_set, notebook)
+
+
+def load_by_naics(naics=[], naics_level=None, geo_level='county', column_set='strict', notebook=True):
+    """
+    Load functional data only for selected naics codes
+    column_set is 'strict': only a subset of columns present in all years
+    will be loaded or 'all': all columns are loaded
+
+    Arguments:
+        :: naics {list of str} -- desired naics codes. {default: []}
+        :: naics_level {int} -- If int, load all naics at that level.
+        If specified, argument naics must then be empty. {default: 2}
+        :: geo_level {str} -- 'msa' or 'county' (default: 'county')
+        :: column_set {str} -- variables to load. 'all': load all available columns.
+        'strict': only load the most essential columns.
+        (default: 'strict')
+        :: notebook {bool} -- is the module running in a Jupyter notebook?
+        The only purpose is to correctly display progress bar.
+        (default: True)
+    Return:
+        pandas Dataframe of desired functional data.
+    """
+    # naics level specified, no naics specified
+    if naics_level:
+        if naics:
+            raise ValueError(
+                'if naics_level is specified, naics must be empty list')
+        # each ? will match with any 1 digit, thus multiply ? by naics_level
+        wild_card = '?' * naics_level
+        glob_path = os.path.join(FUNC_DATA, 'naics', geo_level,
+                                 f'cbp_naics_{geo_level}_{wild_card}.csv')
+        paths = glob.glob(glob_path)
+
+    # naics specified, not level
+    else:
+        if naics_level:
+            raise ValueError(
+                'if naics is specified, naics_level must be None'
+            )
+        paths = []
+        for code in naics:
+            path = os.path.join(FUNC_DATA, 'naics', geo_level,
+                                f'cbp_naics_{geo_level}_{code}.csv')
+            paths.append(path)
+
+    return _load_data_from_paths(paths, geo_level, column_set, notebook)
+
+
+def load_by_geo(geo_codes, geo_level, column_set='strict', notebook=True):
+    """
+    Load functional data only for selected fips or msa codes
+    column_set is 'strict': only a subset of columns present in all years
+    will be loaded or 'all': all columns are loaded
+
+    Arguments:
+        :: geo_codes {list} -- desired fips or msa codes.
+        :: geo_level {str} -- 'msa' or 'county'. Needs to be in agreement with
+        geo_codes.
+        :: column_set {str} -- variables to load. 'all': load all available columns.
+        'strict': only load the most essential columns.
+        (default: 'strict')
+        :: notebook {bool} -- is the module running in a Jupyter notebook?
+        The only purpose is to correctly display progress bar.
+        (default: True)
+    Return:
+        pandas Dataframe of desired functional data.
+    """
+    paths = []
+    for code in geo_codes:
+        path = os.path.join(FUNC_DATA, 'geo', geo_level,
+                            f'cbp_{geo_level}_{code}.csv')
+        paths.append(path)
+    return _load_data_from_paths(paths, geo_level, column_set, notebook)
+
+
+def load_outcome_data():
+    pass
+
+
+def get_variables_by_topic(topics):
+    pass
+
+
+def variable_search(search_terms):
+    pass
